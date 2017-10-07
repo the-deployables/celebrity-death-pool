4c256d59a41450eeb9229852702bbc0271c5028e
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-10-07 13:55:20 -0500

    basic homepage and post routes

--- server.js
+++ server.js
@@ -16,11 +16,25 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // require("./routes/apiRoutes")(app);
 // require("./routes/htmlRoutes")(app);
 
+//Homepage loading
 app.get("/", function(req, res) {
-  // res.send("Welcome to the Star Wars Page!")
+  // res.send("Welcome!")
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+//posting new celeb death guess
+app.post("/add-guess", function(req, res) {
+  var newguess = {
+  	name: req.body.name,
+  	cause: req.body.cause,
+  	deathdate: req.body.deathdate
+  }
+
+  console.log(newguess);
+  // check against database to see if already exists, if no add guess to database
+
+  res.json(newguess);
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
