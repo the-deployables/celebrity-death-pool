eb59ca8cd29c237578402d269ef3faf898919791
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-10-11 11:58:03 -0500

    refined guess object code, started writing form validation

--- server.js
+++ server.js
@@ -1,41 +1,62 @@
+
+//dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var validator = require('express-validator');
 
-// Tells node that we are creating an "express" server
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(validator());
 
+//unclear what these would do in this app...keeping for now
+// require("./routes/apiRoutes")(app); 
+// require("./routes/htmlRoutes")(app); 
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+//user's guess constructor
+function Guess(name, causeofdeath, deathdate) {
+  this.name = name;
+  this.causeofdeath = causeofdeath;
+  this.deathdate = deathdate;
+}
 
-//Homepage loading
+//Homepage route
 app.get("/", function(req, res) {
   // res.send("Welcome!")
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-//posting new celeb death guess
+
+//posting new celeb death guess route
 app.post("/add-guess", function(req, res) {
-  var newguess = {
-  	name: req.body.name,
-  	cause: req.body.cause,
-  	deathdate: req.body.deathdate
-  }
+  
+  //guess object 
+  var userguess = new Guess(req.body.name, req.body.causeofdeath, req.body.deathdate);
 
-  console.log(newguess);
-  // check against database to see if already exists, if no add guess to database
+  //validate form
+  req.checkBody("name", "Enter a living celebrity's name").isEmpty();
+  req.checkBody("causeofdeath", "Enter a prediction for cause of death").isEmpty();
+  req.checkBody("deathdate", "Enter a valid future date.").isAfter();
 
-  res.json(newguess);
+  var errors = req.validationErrors();
+  if (errors) {
+    res.send(errors);
+    return;
+  } else {
+    console.log(userguess);
+  
+  // needs function check against database to see if already exists. If false, add guess to database
+	res.json(userguess);
+  }  
 });
 
+
+//app initilization
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
