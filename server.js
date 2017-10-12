9506bddc906c0e3905a5acbd92d99c2fbe4e8bb3
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-10-11 20:39:08 -0500

    basic add to database written

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 var validator = require('express-validator');
+var mysql = require('mysql');
+var sequelize = require('sequelize');
+
+var db = require("/models");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -13,6 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(validator());
+
+app.set('views', path.join(__dirname, 'views'));  
+app.use(express.static(path.join(__dirname, 'public')));
 
 //unclear what these would do in this app...keeping for now
 // require("./routes/apiRoutes")(app); 
@@ -27,10 +34,8 @@ function Guess(name, causeofdeath, deathdate) {
 
 //Homepage route
 app.get("/", function(req, res) {
-  // res.send("Welcome!")
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+	res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
 
 //posting new celeb death guess route
 app.post("/add-guess", function(req, res) {
@@ -39,22 +44,33 @@ app.post("/add-guess", function(req, res) {
   var userguess = new Guess(req.body.name, req.body.causeofdeath, req.body.deathdate);
 
   //validate form
-  req.checkBody("name", "Enter a living celebrity's name").isEmpty();
-  req.checkBody("causeofdeath", "Enter a prediction for cause of death").isEmpty();
-  req.checkBody("deathdate", "Enter a valid future date.").isAfter();
+  req.checkBody("name", "Enter a living celebrity's name").notEmpty();
+  req.checkBody("causeofdeath", "Enter a prediction for cause of death").notEmpty();
+  req.checkBody("deathdate", "Enter a valid future date.").notEmpty().isAfter();
 
   var errors = req.validationErrors();
-  if (errors) {
-    res.send(errors);
-    return;
-  } else {
-    console.log(userguess);
   
-  // needs function check against database to see if already exists. If false, add guess to database
-	res.json(userguess);
-  }  
-});
+  if (errors) {
+    res.redirect('/');
+  } 
+	else {
+	    console.log(userguess);
+	    res.send('Great Success!');
+	    //check against database to see if already exists. If false, proceed?
+	    //associate user ID to guess?
+	    //write guess to database
 
+	    db.Celebrity.create({
+			name: userguess.name,
+			causeofdeath: userguess.causeofdeath,
+			deathdate: userguess.deathdate
+			}).then(function(guess) {
+			// We have access to the new todo as an argument inside of the callback function
+			res.json(guess);
+			});
+	    //redirect to page with users guesses displayed
+	}
+});
 
 //app initilization
 app.listen(PORT, function() {
